e72d2988666d8a6cab9274c447f30ccefab44f72
zhifeng ou <zhifengou@hotmail.com>
2018-10-13 02:46:19 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -55,5 +55,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  resources: posts, only: :show
+  resources :posts, only: :show
 end
